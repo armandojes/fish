a323df4032909c8d83b08dd89e725654154c7f44
Jesusmaster <armandodejesus678@gmail.com>
2019-08-03 11:48:39 -0500

    integration react js

--- public/index.php
+++ public/index.php
@@ -1,10 +1,16 @@
+<?php
+define('BASE', 'http://localhost');
+?>
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
   <head>
+    <base href="<?= BASE ?>" />
     <meta charset="utf-8">
-    <title></title>
+    <title> armando de jesus </title>
   </head>
   <body>
-    Hello sol
+    <div id="render_target"></div>
+    <script src="/public/app.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="/public/style.css" />
   </body>
 </html>
